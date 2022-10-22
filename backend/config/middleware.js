0d58fb3ec729c3bd77e91a80a06d3069866eaa76
Christopher Maldonado <christopherpr101@gmail.com>
2022-10-22 16:17:24 -0500

    Update middleware

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -2,7 +2,7 @@ module.exports = ({env}) => ({
   settings: {
     cors: {
       enabled: true,
-      origin: env('CORS_ORIGIN', 'http://chma-restaurant-app.s3-website.us-east-2.amazonaws.com')
+      origin: ['*']
     }
   }
 })
